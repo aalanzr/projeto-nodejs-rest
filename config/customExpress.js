f27aaf8a23e5dc44dd4196a88c80c99f231d92cd
Alan Reino <aalanzr@gmail.com>
2022-01-12 23:35:37 -0300

    First Commit

---
+++ config/customExpress.js
@@ -0,0 +1,19 @@
+// Importando os módulos 
+const express = require('express');
+const consign = require('consign'); /* Módulo para agrupar todas as rotas criadas dentro do app => npm install consign */
+const bodyParser = require('body-parser');
+
+module.exports = () => {
+    //Configurando o servidor do jeito que queremos
+    const app = express();
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+    consign()
+        .include('controllers')
+        .into(app);
+
+    return app;
+}
+
